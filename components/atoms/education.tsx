e99a4fbd72d4cb5c1be7f8a5a6fa97cd07e382d9
Ko-Eunseo <koyk0408@naver.com>
2023-02-03 00:59:56 +0900

    education page

--- components/atoms/education.tsx
+++ components/atoms/education.tsx
@@ -1,13 +1,35 @@
 import { ItemProps } from "@/types/types";
-import TeamProjectItem from "./teamProject";
+import common from "@/styles/common/common.module.scss";
+import styles from "@/styles/layouts/education.module.scss";
+import { splitBy } from "@/hooks/filtering";
 
 const EducationItem = (data: ItemProps) => {
-  const { title, content } = data;
+  const { title, content, period, tags, subtitle, subContent } = data;
+  const badges = splitBy(subContent!, "•");
   return (
-    <article>
-      <h1>{title}</h1>
-      <section>{content}</section>
-    </article>
+    <section className={styles.edu_board}>
+      <div>
+        <h3
+          className={`${common.stroke_title} 
+          ${styles.edu_board_title}`}
+        >
+          {title}
+        </h3>
+        <span className={styles.edu_board_span}>{period}</span>
+        <h4 className={styles.edu_board_subtitle}>{subtitle}</h4>
+        <section className={styles.edu_board_content}>{content}</section>
+      </div>
+      <div className={styles.edu_board_badges}>
+        {badges?.map((badge, i) => (
+          <span
+            key={i}
+            className={`${common.round_btn} ${styles.edu_board_span}`}
+          >
+            {badge}
+          </span>
+        ))}
+      </div>
+    </section>
   );
 };
 
---
+++ components/modules/education/swEducation.tsx
@@ -0,0 +1,46 @@
+import EducationItem from "@/components/atoms/education";
+import { filterByTags } from "@/hooks/filtering";
+import useSWR from "swr";
+import common from "@/styles/common/common.module.scss";
+import styles from "@/styles/layouts/education.module.scss";
+import Tab from "@/components/atoms/tab";
+import { useState } from "react";
+
+const SwEducation = () => {
+  const { data, isLoading } = useSWR("/api/notion");
+  const EduDetails = filterByTags(data, "Education");
+  const [curTab, setCurTab] = useState(0);
+  const selectTab = (i: number) => {
+    setCurTab(i);
+  };
+  return (
+    <article className={styles.edu}>
+      <h2 className={styles.edu_title}>SW Education</h2>
+      <ol className={styles.edu_contents}>
+        {EduDetails?.map((edu, id) => (
+          <li key={id}>
+            <Tab
+              curTab={curTab === id}
+              onClick={() => selectTab(id)}
+              subtitle={edu.title}
+            ></Tab>
+          </li>
+        ))}
+      </ol>
+      <div>
+        {EduDetails ? (
+          <EducationItem
+            period={EduDetails[curTab].period}
+            title={EduDetails[curTab].title}
+            subtitle={EduDetails[curTab].subtitle}
+            content={EduDetails[curTab].content}
+            subContent={EduDetails[curTab].subContent}
+            tags={EduDetails[curTab].tags}
+          />
+        ) : null}
+      </div>
+    </article>
+  );
+};
+
+export default SwEducation;
