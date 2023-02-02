c1ad98ebee2166bdc1826a9330923ac43943fed9
Ko-Eunseo <koyk0408@naver.com>
2023-02-03 01:04:39 +0900

    education page

--- components/modules/education.tsx
+++ components/modules/education.tsx
@@ -1,12 +1,29 @@
-import SwEducation from "./education/swEducation";
-import Etc from "./education/careerEtc";
+import useSWR from "swr";
+import EducationItem from "../atoms/education";
+import { ItemProps } from "@/types/types";
+import useFilterByTags from "@/hooks/useFilter";
 
 const Education = () => {
+  const { data, isLoading } = useSWR("/api/notion");
+  const tagName = "education";
+  const educations = useFilterByTags(data, tagName);
+
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
   return (
-    <article id="Education">
-      <SwEducation />
-      <Etc />
-    </article>
+    <>
+      <article>
+        <h1>Education</h1>
+        {educations.map((education: ItemProps, i) => (
+          <EducationItem
+            key={i}
+            title={education.title}
+            content={education.content}
+          />
+        ))}
+      </article>
+    </>
   );
 };
 
